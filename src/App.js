94636d25f93b47b1422aa0bd5ce66153dc15acb5
unknown <ngcebenhleshabangu@gmail.com>
2024-08-05 16:07:40 +0200

    Added HTML structure and components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Card from './Componets/Card/Card';
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome</h1>
+      <Card/>
     </div>
   );
 }
