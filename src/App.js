f9006800afd49a9c73f9ec737e5387c9dc1d462a
unknown <ngcebenhleshabangu@gmail.com>
2024-08-07 08:55:55 +0200

    Created and Added HTML and CSS Basic structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import HomeNavSearch from './Components/HomeNavSearch/HomeNavSearch';
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome</h1>
+     <HomeNavSearch/>
     </div>
   );
 }
