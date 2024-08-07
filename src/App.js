003f3d9e689fe2c03961df349692f3ebf594b240
unknown <ngcebenhleshabangu@gmail.com>
2024-08-07 19:01:54 +0200

    Created and added Basic HTML and CSS structure for List Listing Nav Search

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import HomeNavSearch from './Components/HomeNavSearch/HomeNavSearch';
 import ListListingNavSearch from './Components/ListListingNavSearch/ListListingNavSearch';
+import SelectedListingSearch from './Components/SelectedListingNavSearch/SelectedListingSearch';
 
 function App() {
   return (
     <div className="App">
      {/* <HomeNavSearch/> */}
-     <ListListingNavSearch/>
+     {/* <ListListingNavSearch/> */}
+     <SelectedListingSearch/>
     </div>
   );
 }
