91a3864e60278b2a9336162fa400ac40236433d1
unknown <ngcebenhleshabangu@gmail.com>
2024-08-25 19:32:15 +0200

    Revert "Merge branch 'Home-Page' into Testing"
    This reverts commit d4d460d380ffe6b4cfdb7e269625f6b92c66d561, reversing
    changes made to e3ad840afb750717a306b0176e10f07fb587e397.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,4 @@
 import './App.css';
-import { useEffect, useContext } from 'react';
-import { BrowserRouter, Router, Route,  Redirect, Switch } from "react-router-dom";
-import 'react-datepicker/dist/react-datepicker.css'
-import DatePicker from "react-datepicker";
-import Home from './Pages/Home/Home';
-import Listing from './Pages/Listing/Listing';
-import LogInPage from './Pages/LogIn/LogInPage';
-import Admin from './Pages/Admin/Admin.jsx'
-import axios from 'axios';
-import { LoginContext } from './Context/LogInContext.js';
-
 
 function App() {
 
@@ -28,37 +17,7 @@ function App() {
 const {test, setTest} = useContext(LoginContext)
   return (
     <div className="App">
-  
-    
-      <BrowserRouter>
-        <Switch>
-        
-
-        <Route path='/' exact >
-           <Home/>
-           </Route>
-
-           <Route path = '/home'>
-          <Home/>
-          </Route>
-          
-           <Route path='/listing' >
-           <Listing/>
-           </Route>
-
-           
-
-        </Switch>
-      </BrowserRouter>
-        {/* <Home/> */}
-        {/* <Listing/> */}
-    
-    {/* <select>
-      <option>me </option>
-      <option>her </option>
-      <option>both</option>
-    </select> */}
-        
+      <h1>Welcome</h1>
     </div>
 
   );
